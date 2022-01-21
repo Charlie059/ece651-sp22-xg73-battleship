421f334fd2d914bfee0378e5dd1db859cf7d35c1
xg73 <xg73@duke.edu>
2022-01-21 14:10:27 -0500

    init

---
+++ app/src/test/java/ece651/sp22/xg73/battleship/BoardTextViewTest.java
@@ -0,0 +1,29 @@
+package ece651.sp22.xg73.battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTextViewTest {
+  @Test
+  public void test_display_empty_2by2() {
+    Board b1 = new BattleShipBoard(2, 2);
+    BoardTextView view = new BoardTextView(b1);
+    String expectedHeader = " 0|1\n";
+    assertEquals(expectedHeader, view.makeHeader());
+
+    String expected = expectedHeader + "A  |  A\n" + "B  |  B\n" + expectedHeader;
+    assertEquals(expected, view.displayMyOwnBoard());
+  }
+
+  @Test
+  public void test_invaild_board_size() {
+    Board wideBoard = new BattleShipBoard(11, 20);
+    Board tallBoard = new BattleShipBoard(10, 27);
+    
+    assertThrows(IllegalArgumentException.class,()->new BoardTextView(wideBoard));
+    assertThrows(IllegalArgumentException.class,()->new BoardTextView(tallBoard));
+      
+  }
+
+}
