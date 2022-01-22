6bce7d86c32e307afef99b3bbca28860f4a3b119
xg73 <xg73@duke.edu>
2022-01-22 00:56:04 -0500

    G1T6

---
+++ app/src/main/java/ece651/sp22/xg73/battleship/BasicShip.java
@@ -0,0 +1,41 @@
+package ece651.sp22.xg73.battleship;
+
+public class BasicShip implements Ship<Character> {
+
+  private final Coordinate myLocation;
+
+  public BasicShip(Coordinate myLocation) {
+    this.myLocation = myLocation;
+  }
+
+  @Override
+  public boolean occupiesCoordinates(Coordinate where) {
+    // TODO Auto-generated method stub
+    return where.equals(this.myLocation);
+  }
+
+  @Override
+  public boolean isSunk() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void recordHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean wasHitAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Character getDisplayInfoAt(Coordinate where) {
+    // TODO Auto-generated method stub
+    return 's';
+  }
+
+}
